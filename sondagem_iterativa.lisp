b2675723d1ddf43c249b0f2c7a3d624f5038eb5e
pferreiraa <pedroferreira778@gmail.com>
2018-11-30 01:12:50 +0000

    implemented time logic for multiple sondagens multiplas, lacking function to calculate cost of all shifts in a state

--- sondagem_iterativa.lisp
+++ sondagem_iterativa.lisp
@@ -46,3 +46,20 @@
 
 ;TODO: no nosso caso, como uma sondagem iterativa vai logo dar solução, vamos fazer chamadas a sondagem-iterativa 
 ;      enquanto houver tempo e guardar o melhor resultado de todas as sondagens para devolver
+(defun multiplas-sondagens-iterativas (problema)
+    (let ((tempo-inicio (get-universal-time))
+           (tempo-max-primitido (* 4.5 60)) ;max e 5min, estamos a fazer para 4m30s
+           (melhor-resultado nil)
+           (resultado-sondagem nil)
+           )
+           (loop (while (< (- (get-universal-time) tempo-inicio) tempo-max-primitido))
+                (setf resultado-sondagem (sondagem-iterativa problema))
+                
+                ;TODO: comparar o tempo total do no dado pela sondagem com o melhor atual
+                ;      se for melhor aatualizar o melhor resultado
+                ;      falta funcao que dado um estado, devolve a soma dos custos de cada turno
+            )
+            melhor-resultado
+    )    
+
+)
