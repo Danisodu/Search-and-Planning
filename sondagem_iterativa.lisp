901ca06d1174793e78eaae72d4e176a772a8246f
pferreiraa <pedroferreira778@gmail.com>
2018-12-05 02:28:21 +0000

    sondagem iterativa completa!

--- sondagem_iterativa.lisp
+++ sondagem_iterativa.lisp
@@ -1,5 +1,5 @@
 ;(load "procura.lisp")
-;(load "G008.LISP")
+(load "G008.LISP")
 ;;; 1-samp-aux: estado x problema -> estado
 ;;  recebe um estado e um problema e expande a partir do estado dado de forma aleatoria
 ;;  se encontrar um estado objectivo devolve esse estado, se nao chega ate uma folha nao objectivo e retorna nil
@@ -9,16 +9,22 @@
           (nextStateToExpandIndex)
           (nextStateToExpand)
           (objectivo? (problema-objectivo? problema))
+          (gera-sucessores (car (problema-operadores problema)))
           )
-          
-            
-        (if (funcall objectivo? estado) (return-from-l-samp-aux estado))    
-        (setf sucessores (problema-gera-sucessores problema estado))
+          ;(print "estado:")
+          ;(print estado)
+
+        (if (funcall objectivo? estado)  (return-from 1-samp-aux estado))
+        (setf sucessores (funcall gera-sucessores  estado))
+        ;(print "sucessores")
+       ; (print sucessores)
         
         (cond  ((eq sucessores nil) nil) ;nao e' solucao e e´ folha retornamos nil
                (t (progn 
                         (setf nextStateToExpandIndex (random (list-length sucessores)))
                         (setf nextStateToExpand (nth nextStateToExpandIndex sucessores))
+                        ;(print "next: ")
+                        ;(print nextStateToExpand)
                         (1-samp-aux nextStateToExpand problema)))
         ) 
     )
@@ -28,7 +34,6 @@
 ;;  devolve um estado objectivo ou nil caso nao tenha sido encontrado
 (defun 1-samp (problema)
         (let ((estado-inicial (problema-estado-inicial problema)))  
-             
              (1-samp-aux estado-inicial problema)))
 
  
@@ -37,8 +42,9 @@
 (defun sondagem-iterativa (problema)
     (let ((1-samp-res nil))
     
-        (loop (while (not 1-samp-res))
-            (setf 1-samp-res (1-samp problema)))
+        (loop while (not 1-samp-res) do
+            (setf 1-samp-res (1-samp problema))
+        )
         
         1-samp-res
     )
@@ -54,7 +60,7 @@
            (resultado-sondagem nil)
            (resultado-sondagem-custo)
            )
-           (loop (while (< (- (get-universal-time) tempo-inicio) tempo-max-primitido))
+           (loop while (< (- (get-universal-time) tempo-inicio) tempo-max-primitido) do
            
                 (setf resultado-sondagem (sondagem-iterativa problema))
                 
@@ -72,7 +78,27 @@
                 )
            
             )
+            (print (- (get-universal-time) tempo-inicio))
             melhor-resultado
     )    
 
 )
+
+
+;;--------------------------------
+;;--------TESTES------------------
+;;--------------------------------
+
+
+
+ 
+ ;(setf toAssignTasks '( ('L2 'L1 1 25) ('L1 'L2 34 60) ('L5 'L1 408 447) )) 
+ ;(setf problem (cria-problema (create-state '() toAssignTasks)
+ ;                              (list #'sucessors)
+ ;                              :objectivo? #'is-goal
+ ;                              :estado= #'equality))
+ 
+ ;(print (sondagem-iterativa problem))
+ ;(print (multiplas-sondagens-iterativas problem))
+ 
+ 
